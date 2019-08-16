069649de8a2e72b12342da9c3bcd797fef3df595
Nickita Solovei <https://github.com/NickitaSolovei>
2019-08-16 18:53:26 +0300

    Updates

---
+++ precache-manifest.d6369b1e86f9565a5585a0b50ef196cb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191eff400bb37933a8fd8fa67a0d04bd",
+    "url": "/chat2/index.html"
+  },
+  {
+    "revision": "9727d3971edb0a9d2aee",
+    "url": "/chat2/static/css/main.f43d2cd7.chunk.css"
+  },
+  {
+    "revision": "d4e8b489a7d5adb6a206",
+    "url": "/chat2/static/js/2.285af385.chunk.js"
+  },
+  {
+    "revision": "9727d3971edb0a9d2aee",
+    "url": "/chat2/static/js/main.2de1e4b6.chunk.js"
+  },
+  {
+    "revision": "a032d9587f0c86ae0d4f",
+    "url": "/chat2/static/js/runtime~main.aa1a41e8.js"
+  }
+]);
